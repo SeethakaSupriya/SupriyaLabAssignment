3e53da81527cc4d376ecd51e235ca3c7959375d4
SeethakaSupriya <40495761+SeethakaSupriya@users.noreply.github.com>
2020-11-03 19:53:08 +0530

    Add files via upload

---
+++ LabAssignment05/src/ValidateAge/InvalidAgeException.java
@@ -0,0 +1,16 @@
+package ValidateAge;
+
+public class InvalidAgeException extends Exception {
+	/**
+	 * creating long serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * call constructor of the class and pass message to super class
+	 */
+	public InvalidAgeException() {
+		super("You entered an invalid age ");
+	}
+}
+
---
+++ LabAssignment05/src/ValidateName/InvalidNameException.java
@@ -0,0 +1,13 @@
+package ValidateName;
+
+public class InvalidNameException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	InvalidNameException(String message) {
+		super(message);
+	}
+
+}
